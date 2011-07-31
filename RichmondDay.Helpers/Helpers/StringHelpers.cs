eb8bcefdda37f3c46685718823259ec978b5ec44
Jim Schubert <james.schubert@gmail.com>
2011-07-31 17:17:21 -0400

    Added a trim-to-boundary string helper and a Repeat(char, int) helper

--- RichmondDay.Helpers/Helpers/StringHelpers.cs
+++ RichmondDay.Helpers/Helpers/StringHelpers.cs
@@ -1,21 +1,69 @@
-﻿using System.Text;
-public static class StringHelpers {
-    public static string Trim(this string value, int maxLength) {
+﻿﻿using System.Text;
+using System.Text.RegularExpressions;
+
+public static class StringHelpers
+{
+    const string ELLIPSIS = "…";
+
+    public static string Trim (this string value, int maxLength)
+    {
         if (value.Length <= maxLength)
             return value;
 
-        value = value.Substring(0, maxLength);
-        value = value.Insert(value.Length, "...");
+        value = value.Substring (0, maxLength);
+        value = value.Insert (value.Length, StringHelpers.ELLIPSIS);
 
         return value;
     }
 
-    public static string Repeat(string repeatCharacter, int repeatCount) {
-        StringBuilder sb = new StringBuilder();
+    /// <summary>
+    /// Trims a string to word boundary.
+    /// </summary>
+    /// <returns>
+    /// The trimmed string
+    /// </returns>
+    /// <param name='value'>
+    /// A string to trim
+    /// </param>
+    /// <param name='maxLength'>
+    /// The maximum length of the returned string.
+    /// </param>
+    public static string TrimBoundary (this string value, int maxLength)
+    {
+        if (value.Length <= maxLength)
+            return value;
+
+        string pattern = @"^([^\b]{1," + maxLength.ToString () + @"}(?!\w))";
+        var match = Regex.Match (value, pattern);
+        if (match.Success) {
+            return string.Format ("{0}{1}", match.Captures [0], StringHelpers.ELLIPSIS);
+        } else {
+            return StringHelpers.Trim (value, maxLength);            
+        }
+    }
+
+    public static string Repeat (string repeatCharacter, int repeatCount)
+    {
+        StringBuilder sb = new StringBuilder ();
 
         for (int i = 0; i <= repeatCount; i++)
-            sb.Append("-");
+            sb.Append ("-");
 
-        return sb.ToString();
+        return sb.ToString ();
+    }
+ 
+    /// <summary>
+    /// Repeat the specified character n times.
+    /// </summary>
+    /// <param name='repeat'>
+    /// A single character to repeat
+    /// </param>
+    /// <param name='repeatCount'>
+    /// The number of times to repeat the character
+    /// </param>
+    public static string Repeat (char repeat, int repeatCount)
+    {
+        // This is more efficient than the Repeat(string, int) method
+        return new string (repeat, repeatCount);
     }
 }
